e8e15a21c784735765ce8eb48810a7a9fa5770c3
kubilaybee <kubilaybee@gmail.com>
2022-11-04 17:00:17 +0300

    start project

---
+++ src/main/java/kodlama/io/week5Task/Week5TaskApplication.java
@@ -0,0 +1,13 @@
+package kodlama.io.week5Task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week5TaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kodlama/io/week5Task/Week5TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package kodlama.io.week5Task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2a873a17151f6f01e9777314ba0ca1a06f49a23d
kubilaybee <kubilaybee@gmail.com>
2022-11-07 20:56:26 +0300

    start again

--- src/main/java/kodlama/io/week5Task/dataAccess/abstracts/ProgrammingLanguageRepo.java
+++
@@ -1,7 +0,0 @@
-package kodlama.io.week5Task.dataAccess.abstracts;
-
-import kodlama.io.week5Task.entities.concretes.ProgrammingLanguage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProgrammingLanguageRepo extends JpaRepository<ProgrammingLanguage,Integer> {
-}
---
+++ src/main/java/kodlama/io/week5Task/entities/abstracts/Entity.java
@@ -0,0 +1,4 @@
+package kodlama.io.week5Task.entities.abstracts;
+
+public interface Entity {
+}
